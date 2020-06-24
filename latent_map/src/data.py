3698a0c869e4db4f0895e8851a3aef9462930697
Alex <zhangfx@uchicago.edu>
2020-06-24 16:35:20 -0500

    standardize format

--- latent_map/src/data.py
+++ latent_map/src/data.py
@@ -3,7 +3,9 @@ from typing import List
 from itertools import product
 from matplotlib import pyplot as plt
 import tensorflow as tf
+
 assert tf.__version__.startswith("2")
+
 
 class Data_Factory_Base:
     """
@@ -57,20 +59,21 @@ class Data_Factory:
     """
     Generate data fed to Content Encoder
     """
-    def __init__(self, obj_func, dim:int=3, batch_size:int=40):
-        '''Set parameters'''
+
+    def __init__(self, obj_func, dim: int = 3, batch_size: int = 40):
+        """Set parameters"""
         self.dim = dim
         self.batch_size = batch_size
-        self.raw_data = obj_func(dim=dim, num = 3 * batch_size)
+        self.raw_data = obj_func(dim=dim, num=3 * batch_size)
         self.raw_data = self.raw_data.astype("float32")
-        self.x_train = self.raw_data[:,:-1].astype('float32') 
-        self.y_train = self.raw_data[:,-1]
+        self.x_train = self.raw_data[:, :-1].astype("float32")
+        self.y_train = self.raw_data[:, -1]
         # dataset 2d
         self.compond = list(product(self.raw_data, self.raw_data))
-        self.feed_data = tf.data.Dataset.from_tensor_slices(self.compond).batch(batch_size)
+        self.feed_data = tf.data.Dataset.from_tensor_slices(self.compond).batch(
+            batch_size
+        )
 
-    def get_data(self)->np.ndarray:
-        '''Return generated data'''
-        return self.feed_data,  self.x_train, self.y_train
-
-
+    def get_data(self) -> np.ndarray:
+        """Return generated data"""
+        return self.feed_data, self.x_train, self.y_train
